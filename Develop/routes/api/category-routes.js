2aaace047bb4771b834ecba026c42fc3d824bc77
moassamo <moassermo@gmail.com>
2023-03-17 11:56:35 -0600

    all working, Yaaay!

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -54,7 +54,7 @@ router.post('/', (req, res) => {
     });
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id',(req, res) => {
   // update a category by its `id` value
   Category.update(req.body, {
     where: {
